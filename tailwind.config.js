9845daac6be498d35e23dd4bd1637bb05ecabd9b
juancj1 <118417999+juancj1@users.noreply.github.com>
2022-11-18 20:59:30 +0700

    firts

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF994F',
+                secondary: '#151F3C',
+                nav: 'black',
+                header: '#F6F9FC',
+            },
+        },
+    },
+}
